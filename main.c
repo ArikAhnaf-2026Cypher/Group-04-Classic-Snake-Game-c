0c8584425a699bcfe35b0cca428846975ce23316
Arik Ahnaf <arik.ahnaf.262@northsouth.edu>
2026-07-05 23:33:20 +0600

    Initialize project setup and add Git ignore rules

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-// Group 04 Classic Snake Game Main File 
+// Group 04 Classic Snake Game Main File
+
 #include <stdio.h>
-int main ()
+
+int main (void)
 {
+  printf ("====================================\n"); 
+  printf ("   Group 04 - Classic Snake Game\n");
+  printf ("    Project setup is successful!\n");
+  printf ("====================================\n");
+
   return (0);
 }
