7a3d6ffbd02962b41500ed29b7ae0293f1de4086
jannatulrodela262-dot <jannatul.rodela.262@northsouth.edu>
2026-07-10 19:34:27 +0600

    Update food.c

--- food.c
+++ food.c
@@ -1,49 +1,58 @@
-#include <stdlib.h> //this is the Standard library header file
 
-#include "food.h" 
-#include "board.h"
-#include "snake.h"
-int foodX;
-int foodY;
+    #include <stdlib.h> // Standard library, used for the rand() function
 
-int score = 0;
+#include "food.h"   // Header file for food-related functions
+#include "board.h"  // Header file for the game board
+#include "snake.h"  // Header file for snake variables and functions
 
-void initializeFood (void) 
+int foodX; // Stores the X-coordinate of the food
+int foodY; // Stores the Y-coordinate of the food
+
+int score = 0; // Keeps track of the player's score
+
+// This function generates food at a random valid position on the board
+void initializeFood(void)
 {
     int i;
     int validPosition;
 
-    do 
+    do
     {
-        validPosition = 1;
+        validPosition = 1; // Assume the position is valid at first
 
-        foodX = rand () % BOARD_WIDTH;
-        foodY = rand () % BOARD_HEIGHT;
+        // Generate random coordinates within the board size
+        foodX = rand() % BOARD_WIDTH;
+        foodY = rand() % BOARD_HEIGHT;
 
-        for ( i = 0; i < snakeLength; i ++)
+        // Check if the generated position is on the snake's body
+        for (i = 0; i < snakeLength; i++)
         {
             if (snakeX[i] == foodX && snakeY[i] == foodY)
             {
-                validPosition = 0;
+                validPosition = 0; // Invalid position, food overlaps with snake
                 break;
             }
         }
-    } while (!validPosition);
+
+    } while (!validPosition); // Keep generating until a valid position is found
 }
 
-void drawFood (void) 
+// This function places the food symbol on the game board
+void drawFood(void)
 {
-    board[foodY][foodX] = '*';
+    board[foodY][foodX] = '*'; // '*' represents the food
 }
 
-void checkFoodCollision (void) 
+// This function checks if the snake has eaten the food
+void checkFoodCollision(void)
 {
+    // Compare the snake's head position with the food position
     if (snakeX[0] == foodX && snakeY[0] == foodY)
     {
-        growSnake ();
+        growSnake(); // Increase the snake's length
 
-        score ++;
+        score++; // Increase the player's score
 
-        initializeFood();
+        initializeFood(); // Generate new food after eating
     }
 }
